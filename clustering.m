aa64b9f3e360349d5d721e61aac127bb332737b3
Feoktistov Artem <feoktistov@ragcat-games.com>
2014-04-09 09:22:11 +0700

    Add clustering

---
+++ clustering.m
@@ -0,0 +1,77 @@
+function [resW, resM, resMW, resWD, resMD] = clustering(cn, clusters)
+[k n m] = size(cn);
+
+W = zeros(k,n-1,m);
+M = zeros(k,n,m);
+
+
+for t = 1 : k
+    ct = reshape(cn(t, :, :), n, m);
+    
+    
+    hx = hilbert(ct);
+    
+    hm = abs(hx);
+    %hm = hm / norm(hm);
+    hx = atan((imag(hx)./real(hx)));
+   
+    hw = diff(hx);
+    %hw = hw / norm(hw);
+    
+    M(t, :, :) = M(t, :, :) + reshape(hm, 1, n, m);
+    W(t, :, :) = W(t, :, :) + reshape(hw, 1, n-1, m);
+end;
+
+Mx = zeros(n*m, k);
+for t = 1 : k
+    for l = 1 : n
+        for q = 1 : m
+            Mx(l + q*n, t) = M(t,l,q);
+        end;
+    end;
+end;
+
+Wx = zeros((n-1)*m, k);
+MWx = zeros((n-1)*m, 2*k);
+for t = 1 : k
+    for l = 1 : n-1
+        for q = 1 : m
+            Wx(l + q*(n-1), t) = W(t,l,q);
+            MWx(l + q*(n-1), t) = W(t,l,q);
+            MWx(l + q*(n-1), k + t) = M(t,l,q);
+        end;
+    end;
+end;
+
+Mm = kmeans(Mx, clusters);
+Wm = kmeans(Wx, clusters);
+MWm = kmeans(MWx, clusters);
+
+resM = zeros(n,m);
+resW = zeros(n-1,m);
+resMW = zeros(n-1,m);
+
+for l = 1 : n
+    for q = 1 : m
+        resM(l, q) = Mm(l + q*n,1);
+    end;
+end;
+
+for l = 1 : n-1
+    for q = 1 : m
+        resW(l, q) = Wm(l + q*(n-1),1);
+        resMW(l, q) = MWm(l + q*(n-1),1);
+    end;
+end;
+
+herode = vision.MorphologicalErode;
+hdilate = vision.MorphologicalDilate;
+
+resWD = step(herode,resW) + step(hdilate,resW);
+resMD = step(herode,resM) + step(hdilate,resM);
+
+
+
+
+
+
--- example.m
+++ example.m
@@ -1,8 +1,9 @@
-maxSize = 150; %Максимальный размер изображения
-source = '8'; %Имя изображения. Изоюражение должно лежать в той же папке что и скрипт
-approximation = 'rbf'; %Аппроксимирующая функция
-extension = '.jpg'; %Формат изображения
-image = imread(strcat(source,extension));
+maxSize = 250; 
+for s = 8:14
+source = int2str(s); 
+approximation = 'rbf'; 
+extension = '.jpg';
+image = imread(strcat('images/',source,extension));
 if(length(size(image)) == 3)
     image = sum(image,3);
 end;
@@ -13,34 +14,93 @@ w = min(w,maxSize);
 h = min(h,maxSize);
 image = image(1:w,1:h);
 
+mkdir('data', source);
+
 Image = mat2gray(image);
-name = strcat('data/',source,'.bmp');
+name = strcat('data/',source, '/',source,'.bmp');
 imwrite(Image,name);
 
-[cn,hxn, hyn, hn, r] = HHT(image,approximation);
+
+
+[cn, r] = HHT(image,approximation);
 [k n m] = size(cn);
+
+res = r;
+
+
+
 for t = 1 : k
     ct = reshape(cn(t, :, :), n, m);
     Image = mat2gray(ct);
-    name = sprintf(strcat('data/',source,'_',approximation,'_mode_%d.bmp'), t);
+    name = sprintf(strcat('data/', source, '/',source,'_',approximation,'_mode_%d.bmp'), t);
     imwrite(Image,name);
     
-    ht = reshape(hn(t, :, :), n - 1, m);
-    Image = mat2gray(ht);
-    name = sprintf(strcat('data/',source,'_',approximation,'_mode_%d_hilbert.bmp'), t);
-    imwrite(Image,name);
+    res = res + ct;
     
-    ht = reshape(hxn(t, :, :), n - 1, m);
-    Image = mat2gray(ht);
-    name = sprintf(strcat('data/',source,'_',approximation,'_mode_%d_hilbert_x.bmp'), t);
-    imwrite(Image,name);
+    hx = hilbert(ct);
+    hy = imrotate(hilbert(imrotate(ct,90)),-90);
     
-    ht = reshape(hyn(t, :, :), n - 1, m);
-    Image = mat2gray(ht);
-    name = sprintf(strcat('data/',source,'_',approximation,'_mode_%d_hilbert_y.bmp'), t);
-    imwrite(Image,name);
+    hx = atan((imag(hx)./real(hx)));
+    hy = atan((imag(hy)./real(hy)));
+
+     Image = mat2gray(hx);
+     name = sprintf(strcat('data/',source, '/', source,'_',approximation,'_mode_%d_h_x.bmp'), t);
+     imwrite(Image,name);
+     
+     Image = mat2gray(hy);
+     name = sprintf(strcat('data/',source, '/' , source,'_',approximation,'_mode_%d_h_y.bmp'), t);
+     imwrite(Image,name);
+    
+    
+    hwx = diff(hx);
+    hwy = diff(hy);
+    hw = diff(hx.*hy);
+    
+     Image = mat2gray(hw);
+     name = sprintf(strcat('data/',source, '/',source,'_',approximation,'_mode_%d_freaquency.bmp'), t);
+     imwrite(Image,name);
+     
+     Image = mat2gray(hwx);
+     name = sprintf(strcat('data/',source, '/',source,'_',approximation,'_mode_%d_freaquency_x.bmp'), t);
+     imwrite(Image,name);
+     
+     Image = mat2gray(hwy);
+     name = sprintf(strcat('data/',source, '/',source,'_',approximation,'_mode_%d_freaquency_y.bmp'), t);
+     imwrite(Image,name);
     
 end;
 Image = mat2gray(r);
-name = sprintf(strcat('data/',source,'_',approximation,'_trend.bmp'));
+name = sprintf(strcat('data/', source, '/', source,'_',approximation,'_trend.bmp'));
 imwrite(Image,name);
+
+Image = mat2gray(res);
+name = sprintf(strcat('data/',  source, '/', source,'_',approximation,'_after.bmp'));
+imwrite(Image,name);
+
+
+cl = 5;
+
+[resW, resM, resMW, resWD, resMD] = clustering(cn, cl);
+    
+    Image = mat2gray(resW);
+    name = strcat('data/',source, '/', source,'_resW.bmp');
+    imwrite(Image,name);
+    
+    Image = mat2gray(resM);
+    name = strcat('data/',source, '/', source,'_resM.bmp');
+    imwrite(Image,name);
+
+    Image = mat2gray(resMW);
+    name = strcat('data/',source, '/', source,'_resMW.bmp');
+    imwrite(Image,name);
+    
+    
+    Image = mat2gray(resWD);
+    name = strcat('data/',source, '/', source,'_resWD.bmp');
+    imwrite(Image,name);
+
+    Image = mat2gray(resMD);
+    name = strcat('data/',source, '/', source,'_resMD.bmp');
+    imwrite(Image,name);
+    
+end;
